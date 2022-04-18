3af05e6da30a0ec3a85f12d8e1c4ca793d3cedb6
NAMKUN <happykimnh@icloud.com>
2022-04-18 20:41:12 +0900

    Refactor : 볼과 숫자를 세는 카운터에 대해 좋은 코드 가독성을 위한 변수명 수정 및 주석 추가

--- src/main/java/baseball/game/counter/Counter.java
+++ src/main/java/baseball/game/counter/Counter.java
@@ -15,30 +15,47 @@ public class Counter {
         this.inputNumbers = inputNumbers;
     }
 
+    /**
+     *  생성된 숫자와 입력된 숫자를 비교해서 볼과 스트라이크의 개수를 세는 메서드
+     */
     public GameResult count() {
         int ballCount = 0;
         int strikeCount = 0;
 
         for (int i = 0; i < targetNumbers.size(); i++) {
-            ballCount = isContained(ballCount, i);
+            ballCount = isContained(ballCount, targetNumbers.get(i), inputNumbers.get(i));
             strikeCount = compare(strikeCount, targetNumbers.get(i), inputNumbers.get(i));
         }
 
         return new GameResult(ballCount, strikeCount);
     }
 
-    private int isContained(int count, int num) {
-        if (inputNumbers.contains(targetNumbers.get(num)) && !Objects.equals(targetNumbers.get(num), inputNumbers.get(num))) {
-            count++;
+    /**
+     * 볼 판단 여부
+     * @param ballCount : 세어진 스트라이크 카운트
+     * @param targetNum : 생성된 번호중, 비교되어야 하는 자리수의 번호
+     * @param inputNum : 입력된 번호중, 비교되어야 하는 자시루의 번호
+     * @return : 볼 카운트
+     */
+    private int isContained(int ballCount,  int targetNum, int inputNum) {
+        if (inputNumbers.contains(targetNum) && !Objects.equals(targetNum, inputNum)) {
+            ballCount++;
         }
-        return count;
+        return ballCount;
     }
 
-    private int compare(int count, int a, int b) {
-        if (a == b) {
-            count++;
+    /**
+     *
+     * @param strikeCount : 세어진 스트라이크 카운트
+     * @param targetNum : 생성된 번호중, 비교되어야 하는 자리수의 번호
+     * @param inputNum : 입력된 번호중, 비교되어야 하는 자시루의 번호
+     * @return : 스트라이크 카운트
+     */
+    private int compare(int strikeCount, int targetNum, int inputNum) {
+        if (targetNum == inputNum) {
+            strikeCount++;
         }
-        return count;
+        return strikeCount;
     }
 }
 
